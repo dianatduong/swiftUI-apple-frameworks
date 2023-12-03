9789dd2994a70ea0fa456cc2a2ce73c8d9ba960a
“Diana <dduong013@gmail.com>
2023-12-03 12:21:57 -0600

    initial commit

---
+++ AppleFrameworks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppleFrameworks
+//
+//  Created by Diana Duong on 12/3/23.
+//  Copyright © 2023 Diana Duong. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
